6d6fa979810fe7a60df0f06df9ac760e0ab4ef55
watanuki <im0178sf@ed.ritsumei.ac.jp>
2021-10-03 18:23:26 +0900

    初回プッシュ

---
+++ Assets/Scripts/ClickSad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class ClickSad : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("ノースマイルください");
+        if (GameManager.Instance.point > 0)
+        {
+            GameManager.Instance.point -= 1;
+            GameManager.Instance.Point.fillAmount -= GameManager.Instance.wariai;
+            GameManager.Instance.Point2.fillAmount -= GameManager.Instance.wariai;
+            if (GameManager.Instance.w2 > 0)
+            {
+                GameManager.Instance.w2 -= 5;
+            }
+            else if (GameManager.Instance.w <= 100)
+            {
+                GameManager.Instance.w += 5;
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+}
